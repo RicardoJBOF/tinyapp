9ed2994b1920e2831d820e39703dc4b6eb3fbf06
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-09-02 12:50:58 +0000

    Create register path

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+//adding libraries
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -6,22 +7,23 @@ app.use(bodyParser.urlencoded({extended: true}));
 const  cookieParser = require('cookie-parser')
 app.use(cookieParser())
 
-
+// add EJS
 app.set("view engine", "ejs");
 
+//Start object
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
 
-
+//Functionality to create random keys to the new urls links
 function generateRandomString() {
   let randomKey = Math.random().toString(36).substring(6)
   return randomKey;
 }
 
-
+//To read the file index.ejs
 app.get('/urls', (req, res) => {
   let templateVars = { 
     "urls": urlDatabase,
@@ -31,8 +33,7 @@ app.get('/urls', (req, res) => {
 });
 
 
-
-
+//ro read the file urls_new.ejs
 app.get("/urls/new", (req, res) => {
   let templateVars = {
     username: req.cookies["username"],
@@ -40,7 +41,15 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new", templateVars);
 });
 
-// add an edit request
+//ro read the file register.ejs
+app.get("/register", (req, res) => {
+  let templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("register", templateVars);
+});
+
+// add an edit request (change the address of an already website and keep the previous key)
 app.post("/urls/:shortURL", (req, res) => {
   urlDatabase[req.params.shortURL] = req.body.longURL
   res.redirect(`http://localhost:8080/urls`)
@@ -53,23 +62,24 @@ app.post("/login", (req, res) => {
   res.redirect(`http://localhost:8080/urls`)
 });
 
+// add logout functionality (cleaning cookies)
 app.post("/logout", (req, res) => { 
   res.clearCookie('username');
   res.redirect(`http://localhost:8080/urls`)
 });
 
-
+//create a new Url
 app.post("/urls", (req, res) => {
   let newKey = generateRandomString();
   urlDatabase[newKey] = req.body.longURL;
   res.redirect(`http://localhost:8080/urls/${newKey}`);
 });
 
+//send you back to the urls_index after delete a Url link
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
-
 
 
 //add delete request
@@ -78,14 +88,14 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect(`http://localhost:8080/urls`);
 });
 
-
+//ro read the file urls_show.ejs
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
-
-
+// -----------------------------------------------------------------------------
+//TESTING CODE
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -106,7 +116,9 @@ app.get("/set", (req, res) => {
  app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
  });
+//-------------------------------------------------------------------
 
+//Send message that the server is working
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
