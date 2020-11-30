a0e0cff8eb376171dd472cae9b886928d14bf2e7
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-11-30 18:00:18 -0500

    Refactoring database in a separate file

---
+++ db/database.js
@@ -0,0 +1,31 @@
+// DATABASE OBJECT
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ47lW",
+  },
+};
+
+//USERS OBJECT
+const users = {
+  aJ48lW: {
+    id: "aJ48lW",
+    email: "ricardo@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  "456DeF": {
+    id: "u456DeF",
+    email: "barbosa@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+
+module.exports = {
+  urlDatabase,
+  users
+}
--- express_server.js
+++ express_server.js
@@ -39,32 +39,11 @@ const {
   urlsForUser,
 } = require("./helpers");
 
-// DATABASE OBJECT
-const urlDatabase = {
-  b6UTxQ: {
-    longURL: "https://www.tsn.ca",
-    userID: "aJ48lW",
-  },
-  i3BoGr: {
-    longURL: "https://www.google.ca",
-    userID: "aJ47lW",
-  },
-};
-
-//USERS OBJECT
-const users = {
-  aJ48lW: {
-    id: "aJ48lW",
-    email: "ricardo@example.com",
-    password: "purple-monkey-dinosaur",
-  },
-  "456DeF": {
-    id: "u456DeF",
-    email: "barbosa@example.com",
-    password: "dishwasher-funk",
-  },
-};
-
+// IMPORT DB OBJECTS
+const {
+  urlDatabase,
+  users
+} = require("./db/database.js");
 
 //GET ROUTERS
 app.get("/", (req, res) => {
